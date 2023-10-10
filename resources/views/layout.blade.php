226c416ab4b27338b78ae7b3d8376b752a80942d
dinar <dinar.nugroho.p@ugm.ac.id>
2023-10-10 14:43:13 +0700

    up

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,6 @@
+<h1>ini header </h1>
+<hr>
+@yield('isi')
+
+<hr>
+<h1>ini footer</h1>
---
+++ resources/views/tampil.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('isi')
+    <h4>HALO SMK 2 & SMK 5</h4>
+
+    <h4><?= $kota; ?> <?= $prov; ?></h4>
+    <h4>{{$kota}},{{$prov}}</h4>
+
+    <h5>selamat belajar Laravel</h5>
+@endsection
